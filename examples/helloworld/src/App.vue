e8390703ae76ad8378e3d0714ab4b2b906bcb739
issei miyara <issei_miyara@rebuild-inc.co.jp>
2024-02-17 22:41:05 +0900

    examples/helloworld

---
+++ examples/helloworld/src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import helloworld from "vue-zenkaku-hankaku";
+</script>
+
+<template>
+  <div>
+    <h1>{{ helloworld() }}</h1>
+  </div>
+</template>
---
+++ src/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var helloworld = function () {
+    return 'Hello World';
+};
+exports.default = helloworld;
