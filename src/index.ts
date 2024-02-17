abd6ff9c3f17352bf23c4c3f73045aede1fef5d0
issei miyara <issei_miyara@rebuild-inc.co.jp>
2024-02-18 01:01:13 +0900

    create function that convert hankaku to zenkaku

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,31 @@
-import {ref} from "vue";
+import {ref, watch} from "vue";
 
 export const useZenkakuHankaku = () => {
     const rawText = ref<string>('');
-    const convertedText = ref<string>();
+    const convertedText = ref<string>('');
     const convert = () => {
         convertedText.value = rawText.value.replace(/[Ａ-Ｚａ-ｚ０-９]/g, (s) => {
             return String.fromCharCode(s.charCodeAt(0) - 0xFEE0);
         });
     }
+    watch(rawText, convert)
     return {
         rawText,
-        convertedText,
-        convert
+        convertedText
+    }
+}
+
+export const useHankakuZenkaku= () => {
+    const rawText = ref<string>('');
+    const convertedText = ref<string>('');
+    const convert = () => {
+        convertedText.value = rawText.value.replace(/[A-Za-z0-9]/g, (s) => {
+            return String.fromCharCode(s.charCodeAt(0) + 0xFEE0);
+        });
+    }
+    watch(rawText, convert)
+    return {
+        rawText,
+        convertedText
     }
 }
