f79b095270d54138d5559edf811ee636c0f269f3
issei miyara <issei_miyara@rebuild-inc.co.jp>
2024-02-18 17:27:30 +0900

    typed return value

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,35 @@
-import {ref, watch} from "vue";
+import { ref, watch } from 'vue'
 
-export const useZenkakuHankaku = () => {
-    const rawText = ref<string>('');
-    const convertedText = ref<string>('');
-    const convert = () => {
-        convertedText.value = rawText.value.replace(/[Ａ-Ｚａ-ｚ０-９]/g, (s) => {
-            return String.fromCharCode(s.charCodeAt(0) - 0xFEE0);
-        });
-    }
-    watch(rawText, convert)
-    return {
-        rawText,
-        convertedText
-    }
+interface TextConversionState {
+  rawText: string | undefined
+  convertedText: string | undefined
+}
+export const useZenkakuHankaku = (): TextConversionState => {
+  const rawText = ref<string>('')
+  const convertedText = ref<string>('')
+  const convert = (): void => {
+    convertedText.value = rawText.value.replace(/[Ａ-Ｚａ-ｚ０-９]/g, (s) => {
+      return String.fromCharCode(s.charCodeAt(0) - 0xFEE0)
+    })
+  }
+  watch(rawText, convert)
+  return {
+    rawText: rawText.value,
+    convertedText: convertedText.value
+  }
 }
 
-export const useHankakuZenkaku= () => {
-    const rawText = ref<string>('');
-    const convertedText = ref<string>('');
-    const convert = () => {
-        convertedText.value = rawText.value.replace(/[A-Za-z0-9]/g, (s) => {
-            return String.fromCharCode(s.charCodeAt(0) + 0xFEE0);
-        });
-    }
-    watch(rawText, convert)
-    return {
-        rawText,
-        convertedText
-    }
+export const useHankakuZenkaku = (): TextConversionState => {
+  const rawText = ref<string>('')
+  const convertedText = ref<string>('')
+  const convert = (): void => {
+    convertedText.value = rawText.value.replace(/[A-Za-z0-9]/g, (s) => {
+      return String.fromCharCode(s.charCodeAt(0) + 0xFEE0)
+    })
+  }
+  watch(rawText, convert)
+  return {
+    rawText: rawText.value,
+    convertedText: convertedText.value
+  }
 }
